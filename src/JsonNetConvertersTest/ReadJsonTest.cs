6facbf412bff4be19a7f344f724cac69fab52543
Egor Grishechko <egorgrishechko@gmail.com>
2017-08-15 23:27:30 +0300

    tests

--- src/JsonNetConvertersTest/ReadJsonTest.cs
+++ src/JsonNetConvertersTest/ReadJsonTest.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using JsonNetConverters.Boolean;
+using Newtonsoft.Json;
+using Xunit;
+using Moq;
 
 namespace JsonNetConvertersTest
 {
     public class ReadJsonTest
     {
+        [Theory]
+        [InlineData("1", true)]
+        [InlineData("0", false)]
+        public void ReadJson_ValidInput_SuccessResult(string input, bool expectedResult)
+        {
+            //Arrange
+            var converter = new BooleanConverter();
+            var jsonReaderMock = new Mock<JsonReader>();
+            jsonReaderMock.SetupGet(x => x.TokenType).Returns(JsonToken.String);
+            jsonReaderMock.SetupGet(x => x.Value).Returns("wrongInput");
+
+            //Act
+            //var result = converter.ReadJson()
+        }
     }
 }
