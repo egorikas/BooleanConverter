609595c18f90a00289a4076afafe9cb9c30a4f13
Egor Grishechko <egorgrishechko@gmail.com>
2017-08-15 23:08:19 +0300

    Draft realisation with out tests

--- src/JsonNetConvertersTest/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace JsonNetConvertersTest
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/JsonNetConvertersTest/ReadJsonTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonNetConvertersTest
+{
+    public class ReadJsonTest
+    {
+    }
+}
